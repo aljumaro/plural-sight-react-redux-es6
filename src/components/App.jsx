f320f5b4375e28ad1618f74657af05cd54546589
Juanma <aljumaro@gmail.com>
2017-07-02 12:21:06 +0200

    First react-redux component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import CoursesPage from "./course/CoursesPage";
 
 class App extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <p>Header here...</p>
+                <Header/>
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
+                    <Route exact path="/courses" component={CoursesPage}/>
                     <Route path="/about" component={AboutPage}/>
                 </Switch>
             </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import {render} from "react-dom";
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
 import "./styles/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 render((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('app'));
