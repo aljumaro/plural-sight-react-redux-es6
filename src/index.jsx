02c7c7b0d4d67d6d474fc70d8cc8650c71e27ce4
Juanma <aljumaro@gmail.com>
2017-07-03 18:03:03 +0200

    Load courses from mock api

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,10 @@ import "./styles/styles.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import configureStore from './store/configureStore';
+import {loadCourses} from './actions/courseActions';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render((
     <Provider store={store}>
